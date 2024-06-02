10efcc4a537ead86e664df7208560ee668800ca7
mnuralim <shadownur345.com>
2024-06-02 12:00:08 +0800

    configure vercel.json2

--- server.ts
+++ server.ts
@@ -4,7 +4,6 @@ import app from './src/app'
 dotenv.config()
 
 const port = process.env.PORT || 5000
-
 app.listen(port, () => console.log(`Server is running on port ${port}`))
 
 const server = app
